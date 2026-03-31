786b48653a7bd5b40e16fe3648f8dc5d24878f71
Huy <admin@server>
2026-03-31 08:03:07 +0000

    chore: add missing alt attributes to img tags for improved accessibility

--- resources/views/products/dental-lab-software.blade.php
+++ resources/views/products/dental-lab-software.blade.php
@@ -48,7 +48,7 @@
                 </p>
             </div>
             <div class="apple-split-media fade-in-up md:fade-delay-1">
-                <img src="{{ home_url('wp-content/uploads/2022/08/clinic-dashboard') }}.png" class="apple-split-img rounded-3xl shadow-[0_20px_60px_rgba(0,0,0,0.08)]" loading="lazy">
+                <img src="{{ home_url('wp-content/uploads/2022/08/clinic-dashboard') }}.png" alt="Dental Lab Software dashboard" class="apple-split-img rounded-3xl shadow-[0_20px_60px_rgba(0,0,0,0.08)]" loading="lazy">
             </div>
         </div>
     </div>
