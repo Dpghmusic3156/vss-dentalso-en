9f7b04b10da18851f0af84401902acf5f029520f
dpghmusic3156 <dpghmusic3156@gmail.com>
2026-07-01 03:27:44 -0400

    feat: add page-attributes support to customer post type and set archive order by menu_order

--- app/setup.php
+++ app/setup.php
@@ -358,7 +358,7 @@ add_action('init', function () {
         'archive' => [
             'nopaging' => true,
         ],
-        'supports' => ['title', 'thumbnail', 'custom-fields'],
+        'supports' => ['title', 'thumbnail', 'custom-fields', 'page-attributes'],
     ], [
         'singular' => 'Customer',
         'plural'   => 'Customers',
@@ -383,4 +383,14 @@ add_action('init', function () {
         ],
 
     ]);
+});
+
+/**
+ * Order customer archive by menu_order ASC.
+ */
+add_action('pre_get_posts', function ($query) {
+    if (!is_admin() && $query->is_main_query() && $query->is_post_type_archive('customer')) {
+        $query->set('orderby', 'menu_order');
+        $query->set('order', 'ASC');
+    }
 });
